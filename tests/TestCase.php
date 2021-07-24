bc47167752535d8de31aa90a132f6fae8ade685d
otrsw <otrsw@users.noreply.github.com>
2021-07-24 04:56:02 +0000

    Fix styling

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,8 @@
 namespace Ontherocksoftware\LaravelRedAmberGreen\Tests;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Orchestra\Testbench\TestCase as Orchestra;
 use Ontherocksoftware\LaravelRedAmberGreen\LaravelRedAmberGreenServiceProvider;
+use Orchestra\Testbench\TestCase as Orchestra;
 
 class TestCase extends Orchestra
 {
